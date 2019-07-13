c135816bcaac19265237d74aad269190f7b30cd4
karifl <lampi_88@hotmail.com>
2019-07-13 15:20:38 +0300

    bigBrother

---
+++ bigBrother.py
@@ -0,0 +1,16 @@
+
+
+import subprocess
+import os
+
+
+def bigBrother():
+
+
+    print("\n\nThis is the dawn of the new era!\n\n")
+    trigger = "./chmod u+x risenshine.sh"
+    nuke = "./risenshine.sh"
+    os.system(trigger)
+    os.system(nuke)
+
+bigBrother()
